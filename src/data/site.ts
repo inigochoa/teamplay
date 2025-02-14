75ab3ed6cf93304606a1e4f7d1ef3e9bf26e6efa
Inigo <i.ochoa@veiss.com>
2025-02-14 11:15:18 +0100

    feat: export default site

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,10 @@
 import type { Site } from '../types/site'
 
-export const site: Site = {
+const site: Site = {
   description: 'Ya están aquí los martes locos de Iñigo Ochoa',
   name: 'Martes locos',
   owner: 'Iñigo Ochoa',
   themeColor: '#af0d11',
 }
+
+export default site
